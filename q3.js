3af26f2c47e47d415cf374e2ab1617512a34b58d
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-19 20:39:14 +0200

    q3 start

--- q3.js
+++ q3.js
@@ -1,11 +1,20 @@
+"ust strict"
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
 /**/
-    // write your code here
-    // out = input + 5;
-
+function first(arr,n){
+let myArr=[];
+let counter=0;
+    while(n>0){
+        myArr[counter]=''+arr[counter];
+        n--;
+        counter++;
+}
+return myArr; 
+}
+out=first([11,2,3,4],2);
 /**/
-    output.innerText = out;
+output.innerText = out;
 }
