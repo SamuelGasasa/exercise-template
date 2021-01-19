628d6a44efa9b089f4dbda73d03edd4a05809741
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-19 18:32:38 +0200

    q1 complete

--- q1 copy 5.js
+++ q1 copy 5.js
@@ -1,16 +1,10 @@
-"use strict"
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
 /**/
-let isArr=JSON.parse(input);
-if(Array.isArray(isArr)){
-    out='is array';
-}
-else {
-    out='is not array';
-}
+    // write your code here
+    // out = input + 5;
 
 /**/
     output.innerText = out;
