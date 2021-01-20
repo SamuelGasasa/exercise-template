02c55b740695133404452df29fd50cce6d103d21
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-20 17:51:54 +0200

    q3 answer

--- q3.js
+++ q3.js
@@ -4,17 +4,14 @@ function run() {
     const output = document.getElementById("output");
     let out = "";
 /**/
-function first(arr,n){
-let myArr=[];
-let counter=0;
-    while(n>0){
-        myArr[counter]=''+arr[counter];
-        n--;
-        counter++;
-}
-return myArr; 
-}
 out=first([11,2,3,4],2);
 /**/
 output.innerText = out;
+}
+function first(arr,n){
+let myArr=[];
+for(let i=0;i<n;i++){
+    myArr.push(arr[i]);
+}
+return myArr; 
 }
